f61f51a52e62a00511619d86083230f94f359b36
Derrak Richard <derrak@gmail.com>
2022-09-09 11:48:25 -0700

    add logic to pass in prev. vals if not updated in form

--- src/components/EditInventoryForm.js
+++ src/components/EditInventoryForm.js
@@ -7,16 +7,55 @@ function EditInventoryForm(props) {
 
   function handleEditInventoryFormSubmission(event) {
     event.preventDefault();
-    props.onEditInventory({ names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: inventory.id });
+
+    //Logic to pass in previous values if values were not updated in edit form. Could be DRY'd up
+    //Is this the best place to put thi logic?
+    let name = "";
+    if (event.target.name.value === "") {
+      name = inventory.name;
+    }
+    else { name = "name: event.target.name.value.charAt(0).toUpperCase() + event.target.name.value.slice(1)" }
+
+    let brand = "";
+    if (event.target.brand.value === "") {
+      brand = inventory.brand;
+    }
+    else { brand = "brand: event.target.brand.value.charAt(0).toUpperCase() + event.target.brand.value.slice(1)" }
+
+    let description = "";
+    if (event.target.description.value === "") {
+      description = inventory.description;
+    }
+    else { description = "description: event.target.description.value.charAt(0).toUpperCase() + event.target.description.value.slice(1)" }
+
+    props.onEditInventory({
+      name,
+      brand,
+      description,
+      wholesaleCost: parseFloat(event.target.wholesaleCost.value),
+      scoopPrice: inventory.scoopPrice,
+      scoopsRemaining: inventory.scoopsRemaining,
+      id: inventory.id
+    });
+// Original way of editing an inventory item
+    // props.onEditInventory({
+    //   name: event.target.name.value.charAt(0).toUpperCase() + event.target.name.value.slice(1),
+    //   brand: event.target.brand.value.charAt(0).toUpperCase() + event.target.brand.value.slice(1), 
+    //   description: event.target.description.value.charAt(0).toUpperCase() + event.target.description.value.slice(1),
+    //   wholesaleCost: parseFloat(event.target.wholesaleCost.value),
+    //   scoopPrice: inventory.scoopPrice,
+    //   scoopsRemaining: inventory.scoopsRemaining,
+    //   id: inventory.id
+    // });
   }
 
   return (
     <React.Fragment>
-        <h3>{inventory.name} | {inventory.brand}</h3>
-        <h4><i>{inventory.description}</i></h4>
-        <h4>Wholesale cost: <sup>$</sup>{inventory.wholesaleCost}</h4>
+      <h3>{inventory.name} | {inventory.brand}</h3>
+      <h4><i>{inventory.description}</i></h4>
+      <h4>Wholesale cost: <sup>$</sup>{inventory.wholesaleCost}</h4>
       <ReusableForm
-        formSubmissionHandler={handleEditInventoryFormSubmission} 
+        formSubmissionHandler={handleEditInventoryFormSubmission}
         buttonText="Update Inventory" />
     </React.Fragment>
   );
