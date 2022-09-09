b0306a8c26861b1242e5ef7962d948986036497a
Derrak Richard <derrak@gmail.com>
2022-09-09 10:18:05 -0700

    moved app.js out of components dir

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import InventoryControl from "./components/InventoryControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <InventoryControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
